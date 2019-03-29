905f0efc0923ed401765b750443b032a6b966dc1
Glitch (discord-image-dupe) <none>
2019-03-29 18:20:32 +0000

    🚪🍞 Updated with Glitch

--- bot/permissions.js
+++ bot/permissions.js
@@ -1,27 +1,26 @@
 const dbUtils = require('../utils/db.js')
 const bot = require('../bot/index.js')
 
-function requireRole(role, func) {
+function permission(verify, action) {
   return async function (message) {
-    let serverConfig = await dbUtils.getServerConfig();
-    if (message.isCommand && message.author.id != process.env.BOT_OWNER && message.guild.owner.id != message.author.id && (role == "owner" || (message.member.roles.map(String.toLowerCase).includes(serverConfig[role]) == false && message.member.roles.map(String.toLowerCase).includes(serverConfig.adminRole) == false))) {
+    if (message.isCommand && message.author.id != process.env.BOT_OWNER && message.config && !verify(message, message.config)) {
        return await bot.notify(message, "Insufficient Permissions");
     } else {
-       await func.apply(func, Array.from(arguments));
+       await action.apply(null, Array.from(arguments));
     }
   }
 }
 
 function admin (func) {
-  return requireRole("adminRole", func);
+  return permission((message, config) => message.member.hasPermission('ADMINISTRATOR', false, true, true) || message.member.roles.map(String.toLowerCase).includes(config['adminRole'].toLowerCase()) ,func);
 }
 
 function mod (func) {
-  return requireRole("modRole", func);
+  return permission((message, config) => message.member.hasPermission('MANAGE_GUILD', false, true, true) || message.member.roles.map(String.toLowerCase).includes(config['modRole'].toLowerCase()), func);
 }
 
 function owner (func) {
-  return requireRole("owner", func);
+  return permission((message, config) => message.guild.owner.id == message.author.id, func);
 }
 
 module.exports = {
